5dba47eb2574fbc20cada266580f1d8191b12c73
Your Name <you@example.com>
2024-07-12 15:45:36 -0600

    Se eliminaron 3 lineas basura

--- src/CifradoCesar/Encriptar.java
+++ src/CifradoCesar/Encriptar.java
@@ -9,14 +9,10 @@ public class Encriptar {
     private static char[] alfabeto = {'A', 'B', 'C', 'D', 'E', 'F','G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ','O','P','Q', 'R', 'S', 'T', 'U', 'V', 'W','X', 'Y','Z',
             'a', 'b', 'c', 'd','e', 'f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z'};
 
-    public Encriptar() {
-        // super();
-    }
-
-    public Encriptar(String mensaje) {
+        public Encriptar(String mensaje) {
         this.mensaje = mensaje;
         this.llave = llave;
-    }
+           }
 
     public String getMensaje() {
         return mensaje;
